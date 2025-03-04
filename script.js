7a9ac3f0a4878c95ab7108692847cc37906120ae
nika <nikanozadze1010@gmail.com>
2025-03-04 15:00:00 +0400

    wip: add product to order funbctionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const productsElement = document.querySelector(".products");
+
 const products = [
   {
     image: {
@@ -99,5 +101,65 @@ const products = [
     price: 6.5,
   },
 ];
+const order = [];
 
-console.log(products);
+let result = "";
+products.forEach((product, index) => {
+  const { image, category, name, price } = product;
+  result += `
+        <div class="product">
+          <img
+            src="${image.mobile}"
+            alt=""
+            class="product-img"
+          />
+          <button class="add-btn" value="${name}">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="21"
+              height="20"
+              fill="none"
+              viewBox="0 0 21 20"
+            >
+              <g fill="#C73B0F" clip-path="url(#a)">
+                <path
+                  d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"
+                />
+                <path
+                  d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"
+                />
+              </g>
+              <defs>
+                <clipPath id="a">
+                  <path fill="#fff" d="M.333 0h20v20h-20z" />
+                </clipPath>
+              </defs>
+            </svg>
+            Add to Cart
+          </button>
+          <h3 class="category">${category}</h3>
+          <h2 class="product-name">${name}</h2>
+          <h3 class="price">$${price}</h3>
+        </div>`;
+});
+
+productsElement.innerHTML = result;
+
+const addBtns = document.querySelectorAll(".add-btn");
+const addItemHandler = (productName) => {
+  const clickedProduct = products.find((item) => productName === item.name);
+  const isInOrder = order.find((item) => productName === item.name);
+
+  console.log();
+  if (isInOrder) {
+    isInOrder.amount += 1;
+  } else {
+    order.push({ ...clickedProduct, amount: 1 });
+  }
+};
+
+addBtns.forEach((buttonElement) => {
+  buttonElement.addEventListener("click", () => {
+    addItemHandler(buttonElement.value);
+  });
+});
